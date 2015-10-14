0eb08d972db473034713a6b974c56b21b2283145
Stephen Florian <steezeven@gmail.com>
2015-10-14 15:25:07 -0600

    im in the middle of something

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -2,33 +2,73 @@
 var $infoPane = $('#info-pane')
 var $weather = $('#weather')
 
+var geojsonA;
+$.ajax({
+  method: 'GET',
+  url: '/geojson'
+}).done(function(res){
+  console.log(res.geojson)
+
+  geojsonA = res.geojson;
+  myLayer.setGeoJSON(geojsonA);
+
+  myLayer.on('mouseover', function(e) {
+      e.layer.bindPopup(e.layer.feature.properties.name)
+      e.layer.openPopup();
+  });
+  myLayer.on('mouseout', function(e) {
+      e.layer.closePopup();
+  });
+
+  myLayer.on('click', function(e){
+    map.setView(e.latlng)
+    $infoPane.toggleClass('slideInLeft');
+    var geoId = e.layer.feature.id
+    $('.edit-feature').on('click', function(){
+      $.ajax({
+        url: '/update-feature/' + geoId,
+        method: 'GET'
+      }).done(function(html){
+        $infoPane.html(html)
+        $('.submit-update-submit').on('click', function(e){
+          e.preventDefault()
+          $.ajax({
+            url: '/update-feature/' + $('.submit-update').attr('id'),
+            method: 'POST',
+            data: {
+              id: $('.submit-update').attr('id'),
+              name: $('#name').val(),
+              type: $('#type').val(),
+              _id: $('#_id').val(),
+              thumbnail: $('#thumbnail').val(),
+              isStandard: $('#isStandard').prop('checked'),
+              isSnowOnly: $('#isSnowOnly').prop('checked'),
+              images: $('#more-images').val()
+            }
+          }).done(function(res){
+            console.log(res)
+          })
+        })
+      })
+    })
+    $.ajax({
+      method: 'GET',
+      url: '/' + e.layer.feature.properties.type + '/' + e.layer.feature.properties._id,
+    }).done(function(data){
+      $infoPane.html(data).addClass('slideInLeft');
+      loadAJAX();
+    })
+  })
+})
+
 L.mapbox.accessToken ='pk.eyJ1IjoiY29sb3JhZHVkZSIsImEiOiJjaWY2NnN5MjAwYjVxc21rdTdzdWQwd2NtIn0.4_IhtN06SX3K3moZ1da-cg';
 
 var map = L.mapbox.map('map', 'mapbox.streets')
   .setView([38.638, -107.391], 7);
 
 var myLayer = L.mapbox.featureLayer().addTo(map);
-myLayer.setGeoJSON(geojson);
-myLayer.on('mouseover', function(e) {
-    e.layer.openPopup();
-});
-myLayer.on('mouseout', function(e) {
-    e.layer.closePopup();
-});
 
-myLayer.on('click', function(e){
-  map.setView(e.latlng)
-  console.log(e.layer.feature.properties._id)
-  $infoPane.toggleClass('slideInLeft');
-  $.ajax({
-    method: 'GET',
-    url: '/' + e.layer.feature.properties.type + '/' + e.layer.feature.properties._id,
-  }).done(function(data){
-    $infoPane.html(data).addClass('slideInLeft');
-    loadAJAX();
-  })
-  
-})
+
 
 function loadAJAX(){
   $('.feature-link').off('click').on('click', function(){
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,8 @@ var db = require('monk')(process.env.MONGOLAB_URI || "mongodb://localhost/fourte
 var peaks = db.get('peaks');
 var routes = db.get('routes')
 var trailheads = db.get('trailheads')
-var test = db.get('geoJSON-test')
+var geoJSON = db.get('geoJSON')
+var geoIndividual = db.get('geojson-individual')
 var fs = require('fs')
 var request = require('request')
 
@@ -13,25 +14,20 @@ var request = require('request')
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-//'http://api.openweathermap.org/data/2.5/forecast?lat=' + peak.latitude + '&lon' + peak.longitude
+
 router.get('/peak/:id', function(req, res, next) {
-  console.log('got it')
   peaks.findOne({_id: req.params.id}, function(err, peak){
     if (err) throw err;
     request('http://api.openweathermap.org/data/2.5/forecast?lat=' + peak.latitutde + '&lon=' + peak.longitude + '&cnt=5&APPID=9168266ea473f720024dc6501a3dec27', function (error, response, weather) {
       if (err) throw err;
-      console.log(response)
       if (!error && response.statusCode == 200) {
         var weather = JSON.parse(weather)
-
-        console.log(weather.list)
         routes.find({peakKeys: peak.pkKey}, function(err, routes){
           if (err) throw err;
           trailheads.find({pkKeys: peak.pkKey}, function(err, trailheads){
             if (err) throw err;
             res.render('peak', {peak: peak, routes: routes, trailheads: trailheads, weather: weather.list}, function(err, html){
               if (err) throw err;
-              console.log(html)
               res.send(html)
             })
           })
@@ -73,5 +69,115 @@ router.get('/route/:id', function(req, res, next) {
     })
   });
 });
+
+
+router.get('/geoJSON', function(req, res){
+  geoJSON.findOne({}, function(err, geoJSON){
+    res.send(geoJSON);
+  })
+})
+
+router.get('/update-feature/:id', function(req, res){
+  res.render('update-form', {id: req.params.id}, function(err, html){
+    if (err) throw err;
+    res.send(html)
+  })
+})
+router.post('/update-feature/:id', function(req, res){
+  console.log('++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++')
+  // geoIndividual.findOne({id: req.body.id}, function(err, result){
+  //   console.log(result)
+  // })
+  // geoJSON.findOne({_id: req.body._id}, function(err, result){
+  //   console.log(result)
+  // })
+  console.log('++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++')
+
+  geoIndividual.findOne({id: req.body.id}, function(err, doc){
+    var replacement = doc
+    replacement._id = req.body._id
+    replacement.properties.name = req.body.name
+    replacement.properties.type = req.body.type 
+    delete replacement.properties.desc
+    delete replacement.properties.ele
+    delete replacement.properties.title
+    delete replacement.properties.sym
+    geoIndividual.remove({id: req.body.id})
+    geoIndividual.insert(replacement)
+
+    if (req.body.type === 'peak'){
+      peaks.findOne({ _id: req.body._id}, function(err, result){
+        console.log(result)
+        result.thumbnail = req.body.thumbnail
+        if (req.body.type === 'route'){
+          result.isStandard = req.body.isStandard
+          result.isSnowOnly = req.body.isSnowOnly
+        } 
+        if (req.body.images){
+          result.images = req.body.images.split(' ')
+        }
+        peaks.remove({_id: req.body._id})
+        peaks.insert(result)
+      })
+    }
+    
+  })
+ 
+  
+
+  // if (req.body.type === 'route'){
+  //   geoJSON.findAndModify({ _id: req.body._id }, { 
+  //     $set: {
+  //       isStandard: req.body.isStandard,
+  //       isSnowOnly: req.body.isSnowOnly
+  //     }
+  //   }, function(err){ if (err) throw err })
+      
+  // } 
+
+  // geoJSON.findAndModify({query: {_id: req.body._id}, update: {$push : {images: req.body.images.split(',')}}}, function(err){ if (err) throw err })
+
+  console.log('++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++')
+
+
+  // geoIndividual.findOne({id: req.body.id}, function(err, result){
+  //   console.log(result)
+  // })
+  // geoJSON.findOne({_id: req.body._id}, function(err, result){
+  //   console.log(result)
+  // })
+  console.log('++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++')
+
+})
+
+
+
+
+
+
+
+
+
+// // Creates database of individual geoJSON features
+// ...geoJSON.findOne({}, function(err, data){
+//   if (err) throw err;
+//   data.geojson.features.forEach(function(feature){
+//     geoIndividual.insert(feature);
+//   })
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 module.exports = router;
